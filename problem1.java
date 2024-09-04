41a0e19cdc9d1bde36888f56623d53a33e09d173
Awesometimei <dtnguyen194@student.hbuhsd.edu>
2024-09-04 09:56:38 -0700

    fortnite

--- problem1.java
+++ problem1.java
@@ -1,1 +1,8 @@
-// Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        String favFood = "chicken";
+        System.out.println("I like to eat " + favFood + " as well!");
+    }
+}
--- problem2.java
+++ problem2.java
@@ -1,1 +1,11 @@
-// Create your class here; don't forget the main method
+public class problem2
+{
+    public static void main(String[] args)
+    {
+        String name1 = "Danny";
+        String name2 = "Justin";
+        String name3 = "Philip";
+        String name4 = "Raymond";
+        System.out.println(name4 + " " + name2 + " " + name1 + " " + name3);
+    }
+}
